06b0f1b22481f0a2f8f2b38f4380d8279e24a679
子崇 <gs109845@alibaba-inc.com>
2025-09-11 10:25:19 +0800

    test dev

--- com/TagRecordServiceClient.java
+++ com/TagRecordServiceClient.java
@@ -3,23 +3,15 @@ public class TagRecordServiceClient {
     private static CacheMap stringTagRecordDoMap = new CacheMap();
 
     public String getTagRecordDo(String code){
-        String tagRecord  = stringTagRecordDoMap.get(code);
+        CacheMap cacheMap = new CacheMap();
+        String tagRecord  = cacheMap.get(code);
         if(tagRecord != null){
             return tagRecord;
-        }
-        if(tagRecord != null){
+        }else{
+            tagRecord = code+"_"+System.currentTimeMillis();
             // 本地缓存
             stringTagRecordDoMap.put(code, tagRecord);
             return tagRecord;
         }
-        try{
-
-                // 本地缓存
-            stringTagRecordDoMap.put(code, code+"_"+System.currentTimeMillis());
-            return stringTagRecordDoMap.get(code);
-        }catch(Exception e){
-            return null;
-        }
-
     }
 }
